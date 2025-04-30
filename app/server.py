53eed62efd3b71cd4e3541a26eaa4aaaa17a9b03
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2025-04-30 11:34:41 +0300

    fix(server): update CORS origins to match frontend configuration

--- app/server.py
+++ app/server.py
@@ -8,8 +8,8 @@ from app.api.graphql.router import graphql_router
 app = FastAPI(title="Analytic Project API")
 
 origins = [
-    "http://localhost:3001",
-    "http://127.0.0.1:3001",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 app.add_middleware(
@@ -23,6 +23,7 @@ app.add_middleware(
 # Include routers
 app.include_router(auth_router.router, prefix="/api/v1", tags=["auth"])
 app.include_router(graphql_router, prefix="/graphql", tags=["graphql"])
+
 
 @app.get("/", tags=["Root"])
 async def read_root():
